c7705f63ce9377fe33f756698bdd3126b622b481
thejaylajones <janeissaj@gmail.com>
2025-06-09 12:04:32 -0400

    demo work in class

---
+++ Demo1/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+        ObjectMapper om = new ObjectMapper();
+    }
+}
